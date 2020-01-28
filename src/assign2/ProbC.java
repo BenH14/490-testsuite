5a112bcb491b3ec140f29c9c68dc97111f5f3da2
BenH14 <benjamin.henaghan@gmail.com>
2020-01-27 16:43:08 -0800

    Add solution files

---
+++ src/assign2/ProbC.java
@@ -0,0 +1,21 @@
+package assign2;
+
+import java.io.*;
+
+public class ProbC {
+
+    public static void main(String[] args) throws IOException {
+        problemA(System.in, System.out);
+    }
+
+    public static void problemA(InputStream in, OutputStream out) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        PrintWriter writer = new PrintWriter(out);
+
+        // Solve problem
+
+        reader.close();
+        writer.close();
+    }
+
+}
