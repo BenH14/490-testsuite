4aecece7a3905f313acff5dc69690bc261a0cbb9
BenH14 <benjamin.henaghan@gmail.com>
2020-02-06 18:22:07 -0800

    Added junit reader for testfiles

--- src/testcore/TestExecutor.java
+++ src/testcore/TestExecutor.java
@@ -3,14 +3,18 @@ package testcore;
 import assign2.ProbA;
 import org.junit.jupiter.api.Assertions;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class TestExecutor {
 
     public static void runTest(Solution solution, long timeLimit, String input, String expected) {
+        runTest(solution, timeLimit, input, expected, "");
+    }
+
+    public static void runTest(Solution solution, long timeLimit, String input, String expected, String testname) {
         InputStream instream = new ByteArrayInputStream(input.getBytes());
         OutputStream outputStream = new java.io.ByteArrayOutputStream();
 
@@ -22,10 +26,26 @@ public class TestExecutor {
         }
         var t = System.currentTimeMillis() - startTime;
 
-        String result = outputStream.toString();
+        String result = outputStream.toString().trim();
 
-        Assertions.assertEquals(expected, result, "Incorrect Output");
+        Assertions.assertEquals(expected.trim(), result, "Incorrect Output");
         Assertions.assertTrue(t < timeLimit, "Time Limit Exceeded");
+        if (testname.length() > 0) {
+            System.out.println("Test Name = " + testname);
+        }
         System.out.println(String.format("Completed in %dms", t));
+    }
+
+    public static void runTestFiles(Solution solution, long timelimit, String dirpath) throws IOException {
+        String indir = dirpath + "\\in";
+        String outdir = dirpath + "\\std";
+        File inputFiles = new File(indir);
+        for (String fname: inputFiles.list()) {
+            var infile = Paths.get(indir + "\\" + fname);
+            var outfile = Paths.get(outdir+ "\\" + fname.split("\\.")[0] + ".std");
+            String input = new String(Files.readAllBytes(infile));
+            String output = new String(Files.readAllBytes(outfile));
+            runTest(solution, timelimit, input, output, fname);
+        }
     }
 }
